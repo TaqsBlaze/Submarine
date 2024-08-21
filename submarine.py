93f0c971161d43cbd805a70bc3da16b59691121f
Tanaka Chinengundu <tanakah30@gmail.com>
2024-08-21 11:31:32 +0200

    Added search method to the SubMarine class

--- submarine.py
+++ submarine.py
@@ -22,20 +22,27 @@ class ImportModule:
 
 
 
+
+
 class Submarine:
 
     def __init__(self, target):
         self.target = target
         self.sub_domain_db = None
+        self.found = []
 
 
     def clean_string(self):
 
         # Remove protocol
         if self.target.startswith("https://") or self.target.startswith("http://"):
-            disect = self.target.split("//")
+            protocol_disect = self.target.split("//")
             no_protocol = disect[1]
             self.target = no_protocol
+        # Removing www subdomain
+        if self.target.startswith("www."):
+            subdomain_disect = self.target.split("www.")[1]
+            self.target = subdomain_disect
 
         return self.target
 
@@ -43,11 +50,23 @@ class Submarine:
     def load_sub_db(self):
         try:
             with open("sublist/sublist.txt","r") as sublistdb:
-                subdomains = sublistdb.readlines().strip()
+                subdomains = sublistdb.readlines()
                 self.sub_domain_db = subdomains
         except Exception as error:
             print("[+] Failed to load subdomain database")
+            exit()
 
 
     def sector_search(self):
-        pass
+        
+        for subdomain in self.sub_domain_db:
+
+            target = f"{self.sub_domain_db}{self.target}"
+
+            response = requests.get(target)
+
+            if response.status_code == 200:
+                self.found.append(target)
+            else:
+                continue
+
